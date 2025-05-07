d0aa47583d64a200ac454967f4c984da6841a709
vozaldi <vozaldi@gmail.com>
2025-05-07 23:01:10 +0700

    fix: Next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,38 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pejuang.berkarirasn.id',
+        port: '',
+        pathname: '/**',
+      }, {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      }, {
+        protocol: 'https',
+        hostname: 'lms.pekade.my.id',
+        port: '',
+        pathname: '/**',
+      }, {
+        protocol: 'https',
+        hostname: 'asn.pekade.my.id',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  sassOptions: {
+    // implementation: 'sass-embedded',
+    silenceDeprecations: [
+      'legacy-js-api', 'import', 'mixed-decls', 'color-functions', 'global-builtin'
+    ],
+  },
 };
 
 export default nextConfig;
